80fcd6cadd66567dce755c61def25bd67dbfd2a1
Johannes <84457538+p-jeff@users.noreply.github.com>
2023-03-22 10:10:09 +0100

    select works !

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import { MapControls, OrbitControls, Plane, Torus } from '@react-three/drei';
+import { MapControls, Plane, Select, useSelect, Edges } from '@react-three/drei';
+import { useState } from 'react';
+
 
 function degreeToRad(degrees) {
   return degrees * (Math.PI / 180);
 }
 
-function App() {
+// Thanks chatGPT
+//this function checks if an object is selected -> and returns the tag of the selected object
+
+function logObjectIfDefined(obj) {
+  let current
+  if (typeof obj === 'object' && obj !== null) {
+    current = obj.tag
+    // console.log(obj);
+    return current
+  } else {
+    //console.log('Object is not defined');
+    return 'none'
+  }
+}
+
+
+// useSelect needs to be in a Child of <Select> 
+function MyTorus({ ...props }) {
+  const selected = useSelect()
+
+  let current = logObjectIfDefined(selected[0])
+  // tag is going to be identifier of shown text
+
+  let isSelected = current === props.tag
+
+
   return (
-    <div className='App' >
-      <Canvas camera={{ position: [5, 5, 0] }}>
+    <mesh {...props}>
+      <torusGeometry />
+      <meshNormalMaterial />
+
+      {/*
+      Replace with bounding box
+      <Edges visible={isSelected} scale={1.1} renderOrder={1000}>
+        <meshBasicMaterial transparent color="#333" depthTest={false} />
+  </Edges>
+  */}
+    </mesh>
+  )
+}
+/* */
+
+function App() {
+  const [selected, setSelected] = useState([])
+
+  return (
+    <div className='App'>
+      <Canvas camera={{ position: [5, 5, 0] }} >
         <MapControls />
         <ambientLight intensity={0.1} />
         <Plane args={[10, 10, 10]} rotation={[degreeToRad(-90), 0, 0]} />
-        <Torus position={[0, 0.4, 0]} rotation={[degreeToRad(-90), 0, 0]}>
-          <meshNormalMaterial />
-        </Torus>
+        <Select box multiple onChange={setSelected}>
+          <MyTorus scale={0.9} position={[-1, 0, 0]} tag={22} />
+          <MyTorus scale={0.9} position={[1, 0, 0]} tag={23} />
+        </Select>
       </Canvas>
-
-    </div >
+    </ div >
   );
 }
 
